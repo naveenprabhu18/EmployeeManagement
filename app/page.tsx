d89d153bb5ce5c11b100718076af0e741eefe8fa
naveen <naveenviki13@gmail.com>
2026-04-19 20:13:41 +0530

    "Test2"

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <h1>Employee Management-pr</h1>
      <h2>Test</h2>
+     <h2>test 2</h2>
     </div>
   );
 }
